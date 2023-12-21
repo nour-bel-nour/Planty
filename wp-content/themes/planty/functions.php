31b91ed3c5682b1c7b8ac6c6c3491ca73078d080
Nourdine Belkhadir <140583027+nour-bel-nour@users.noreply.github.com>
2023-12-21 22:56:36 +0100

    ajustert lien Admin

--- wp-content/themes/planty/functions.php
+++ wp-content/themes/planty/functions.php
@@ -7,3 +7,15 @@ function theme_enqueue_styles()
     wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/css/theme.css', array(), filemtime(get_stylesheet_directory() . '/css/theme.css'));
 }
 
+
+add_filter( 'wp_nav_menu_items', 'add_admin_menu', 10,2);
+ function add_admin_menu ( $items, $args ) {
+		 // utilisateur connecté ou non.  
+         /*echo $args->theme_location;*/
+			if (is_user_logged_in() && $args->theme_location == 'primary') {
+		  // Donner accès à un menu admin et l'URL du tableau de bord WP et afficher le lien dans le menu
+		 $items .= '<li class="menu-admin menu-item"><a href="'. admin_url() .'">Admin</a></li>';
+		}
+		
+		return $items;
+	}
